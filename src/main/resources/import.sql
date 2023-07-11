55206b7acf12270846e42b7c10b81f702b22d0a5
calliauc <clement@calliau.io>
2023-07-11 23:35:21 +0200

    test single properties

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,13 +1,13 @@
 -- This file allow to write SQL commands that will be emitted in test and dev.
 -- The commands are commented as their support depends of the database
-insert into episode (id, title, description) values(1, 'episode1', 'description');
-insert into episode (id, title, description) values(2, 'episode2', 'description');
-insert into episode (id, title, description) values(3, 'episode3', 'description');
-insert into episode (id, title, description) values(4, 'episode4', 'description');
-
-insert into player (id, name, pseudo) values(1, 'Clement', 'Makhai');
-insert into player (id, name, pseudo) values(2, 'Alex', '');
-insert into player (id, name, pseudo) values(3, 'Timo', 'Nox');
-
-alter sequence episode_seq restart with 5;
-alter sequence player_seq restart with 4;
+-- insert into episode (id, title, description) values(1, 'episode1', 'description');
+-- insert into episode (id, title, description) values(2, 'episode2', 'description');
+-- insert into episode (id, title, description) values(3, 'episode3', 'description');
+-- insert into episode (id, title, description) values(4, 'episode4', 'description');
+--
+-- insert into player (id, name, pseudo) values(1, 'Clement', 'Makhai');
+-- insert into player (id, name, pseudo) values(2, 'Alex', '');
+-- insert into player (id, name, pseudo) values(3, 'Timo', 'Nox');
+--
+-- alter sequence episode_seq restart with 5;
+-- alter sequence player_seq restart with 4;
