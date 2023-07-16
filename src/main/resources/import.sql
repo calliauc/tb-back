07212289001b8b82c9f6d78ef768fd409ec22116
calliauc <clement@calliau.io>
2023-07-16 22:41:16 +0200

    classes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,13 +1,13 @@
 -- This file allow to write SQL commands that will be emitted in test and dev.
 -- The commands are commented as their support depends of the database
-insert into episode (id, title, description) values(1, 'episode1', 'description');
-insert into episode (id, title, description) values(2, 'episode2', 'description');
-insert into episode (id, title, description) values(3, 'episode3', 'description');
-insert into episode (id, title, description) values(4, 'episode4', 'description');
+insert into joueur (id, name, pseudo) values(1, 'Clement', 'Makhai');
+insert into joueur (id, name, pseudo) values(2, 'Alex', '');
+insert into joueur (id, name, pseudo) values(3, 'Timo', 'Nox');
 
-insert into player (id, name, pseudo) values(1, 'Clement', 'Makhai');
-insert into player (id, name, pseudo) values(2, 'Alex', '');
-insert into player (id, name, pseudo) values(3, 'Timo', 'Nox');
+alter sequence joueur_seq restart with 4;
 
-alter sequence episode_seq restart with 5;
-alter sequence player_seq restart with 4;
+insert into classe (id, name) values(1, 'Equipier');
+insert into classe (id, name) values(2, 'Soigneur');
+insert into classe (id, name) values(3, 'Cuirassier');
+
+alter sequence classe_seq restart with 4;
